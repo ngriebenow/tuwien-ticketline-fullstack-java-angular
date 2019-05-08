bb665e81fe9c5e475a17fcffa7bb74aa99997f9a
Nicolas Griebenow <nicolas.griebenow@gmx.at>
2019-05-08 18:05:00 +0200

    [issue #35693] Add event component

---
+++ frontend/src/app/components/event/event.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {EventService} from '../../services/event.service';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.scss']
+})
+export class EventComponent implements OnInit {
+
+  constructor(private eventService: EventService) {}
+  private event: Event;
+
+
+  ngOnInit() {
+    this.loadEvent(3);
+  }
+
+
+  /**
+   * Loads the text of message and update the existing array of message
+   * @param id the id of the message which details should be loaded
+   */
+  loadEvent(id: number) {
+    this.eventService.getEventById(id).subscribe(
+      event => this.event = event);
+  }
+
+}
