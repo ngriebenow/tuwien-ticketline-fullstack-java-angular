5ae6f76b1aefb1b851ba61435a3662c7de5b8d37
Benjamin Zinschitz <b.zinschitz@gmail.com>
2019-05-10 14:40:57 +0200

    [issue #35690] Add missing @Entity annotation to PriceCategory

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/PriceCategory.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/PriceCategory.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.sepm.groupphase.backend.entity.PriceCategory.Builder;
 import java.awt.Color;
 import java.util.Objects;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
+@Entity
 public class PriceCategory {
 
   @Id
