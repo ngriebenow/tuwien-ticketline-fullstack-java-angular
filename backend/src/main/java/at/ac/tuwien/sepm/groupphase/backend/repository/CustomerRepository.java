ba3623dba2f568bb9ac1b9b383509d186acf01ff
Benjamin Zinschitz <b.zinschitz@gmail.com>
2019-05-10 16:01:00 +0200

    [issue #35689] Configure Customer persistance

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package at.ac.tuwien.sepm.groupphase.backend.repository;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
---
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/repositorytest/CustomerRepositoryTest.java
@@ -0,0 +1,43 @@
+package at.ac.tuwien.sepm.groupphase.backend.repositorytest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Customer;
+import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepm.groupphase.backend.repository.CustomerRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+public class CustomerRepositoryTest {
+
+  private Customer CUSTOMER_1 =
+      new Customer.Builder()
+          .name("Harald")
+          .surname("Tester")
+          .email("haral.tester@gmail.com")
+          .build();
+
+  @Autowired CustomerRepository customerRepository;
+
+  @Before
+  public void setUp() {
+    customerRepository.save(CUSTOMER_1);
+  }
+
+  @Test
+  public void givenCustomerSaved_whenFindCustomerById_thenReturnCustomer() {
+    Customer customer = customerRepository.findById(CUSTOMER_1.getId()).get();
+    assertThat(customer).isEqualTo(CUSTOMER_1);
+  }
+
+  @Test(expected = NotFoundException.class)
+  public void givenCustomerSaved_whenFindUnknownCustomerById_thenThrowNotFoundException() {
+    customerRepository.findById(-1L).orElseThrow(NotFoundException::new);
+  }
+}
