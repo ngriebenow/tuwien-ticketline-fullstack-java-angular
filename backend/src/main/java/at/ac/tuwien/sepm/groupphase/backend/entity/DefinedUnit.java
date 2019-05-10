3c168735a21e8c5cecd391656c914b1eac6b0b33
Benjamin Zinschitz <b.zinschitz@gmail.com>
2019-05-10 16:01:29 +0200

    [issue #35689] Configure DefinedUnit persistance

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/DefinedUnit.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/DefinedUnit.java
@@ -1,20 +1,55 @@
 package at.ac.tuwien.sepm.groupphase.backend.entity;
 
+import com.google.common.base.Objects;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 
+@Entity
 public class DefinedUnit {
 
+  private static final String ID_SEQUENCE_NAME = "seq_defined_unit_id";
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = ID_SEQUENCE_NAME)
+  @SequenceGenerator(name = ID_SEQUENCE_NAME)
   private Long id;
 
+  @Column(nullable = false)
   private int capacityFree;
 
+  @ManyToOne
+  @JoinColumn(nullable = false)
   private Unit unit;
 
+  @OneToMany(mappedBy = "definedUnit")
   private List<Ticket> ticket;
 
+  @ManyToOne
+  @JoinColumn(nullable = false)
   private PriceCategory priceCategory;
 
+  @ManyToOne
+  @JoinColumn(nullable = false)
   private Performance performance;
+
+  public DefinedUnit() {}
+
+  private DefinedUnit(Builder builder) {
+    setId(builder.id);
+    setCapacityFree(builder.capacityFree);
+    setUnit(builder.unit);
+    setTicket(builder.ticket);
+    setPriceCategory(builder.priceCategory);
+    setPerformance(builder.performance);
+  }
 
   public Long getId() {
     return id;
@@ -62,5 +97,92 @@ public class DefinedUnit {
 
   public void setPerformance(Performance performance) {
     this.performance = performance;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    DefinedUnit that = (DefinedUnit) obj;
+    return capacityFree == that.capacityFree
+        && Objects.equal(id, that.id)
+        && Objects.equal(unit, that.unit)
+        && Objects.equal(ticket, that.ticket)
+        && Objects.equal(priceCategory, that.priceCategory)
+        && Objects.equal(performance, that.performance);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id, capacityFree, unit, ticket, priceCategory, performance);
+  }
+
+  @Override
+  public String toString() {
+    return "DefinedUnit{"
+        + "id="
+        + id
+        + ", capacityFree="
+        + capacityFree
+        + ", unit="
+        + unit
+        + ", ticket="
+        + ticket
+        + ", priceCategory="
+        + priceCategory
+        + ", performance="
+        + performance
+        + '}';
+  }
+
+  public static final class Builder {
+
+    private Long id;
+    private int capacityFree;
+    private Unit unit;
+    private List<Ticket> ticket;
+    private PriceCategory priceCategory;
+    private Performance performance;
+
+    public Builder() {
+    }
+
+    public Builder id(Long val) {
+      id = val;
+      return this;
+    }
+
+    public Builder capacityFree(int val) {
+      capacityFree = val;
+      return this;
+    }
+
+    public Builder unit(Unit val) {
+      unit = val;
+      return this;
+    }
+
+    public Builder ticket(List<Ticket> val) {
+      ticket = val;
+      return this;
+    }
+
+    public Builder priceCategory(PriceCategory val) {
+      priceCategory = val;
+      return this;
+    }
+
+    public Builder performance(Performance val) {
+      performance = val;
+      return this;
+    }
+
+    public DefinedUnit build() {
+      return new DefinedUnit(this);
+    }
   }
 }
