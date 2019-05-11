9c41e8c019f36a500c36cd081f14fd8b768ec267
Jakob Lindner <jakob123lindner@gmail.com>
2019-05-11 15:47:13 +0200

    [issue #35687] Add hall plan mockup to hall creation

--- frontend/src/app/components/hall-creation-plan/hall-creation-plan.component.ts
+++ frontend/src/app/components/hall-creation-plan/hall-creation-plan.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HallCreationService } from '../../services/hall-creation.service';
+import {Point} from '../../dtos/Point';
+import {max, min} from "rxjs/operators";
 
 @Component({
   selector: 'app-hall-creation-plan',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HallCreationPlanComponent implements OnInit {
 
-  constructor() { }
+  hallSize: Point;
+  seats: Point[];
+
+  constructor(private hallCreationService: HallCreationService) {
+    this.getSeats();
+  }
 
   ngOnInit() {
   }
 
+  getHallSize(): void {
+    this.hallSize = this.hallCreationService.getHallSize();
+  }
+
+  getSeats(): void {
+    this.seats = this.hallCreationService.getSeats();
+    //this.seats = [];
+    //this.seats.push(new Point(4, 5));
+    //this.seats.push(new Point(1, 1));
+  }
+
+  calcPositionLeft(n: number) {
+    let left: number = this.seats[n].coordinateX * this.unitSize() - this.unitSize() + 20;
+    if (this.hallSize.coordinateY > this.hallSize.coordinateX) {
+      left = left + (this.hallSize.coordinateY - this.hallSize.coordinateX) * this.unitSize() / 2;
+    }
+    return left + 'px';
+  }
+
+  calcPositionTop(n: number) {
+    return (this.seats[n].coordinateY * this.unitSize() - this.unitSize() + 80) + 'px';
+  }
+
+  calcSize() {
+    this.getHallSize();
+    return (this.unitSize() - 20) + 'px';
+  }
+
+  unitSize() {
+    this.getHallSize();
+    return Math.min(780 / this.hallSize.coordinateX, 780 / this.hallSize.coordinateY);
+  }
 }
---
+++ frontend/src/app/services/hall-creation.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {Hall} from '../dtos/hall';
+import {Point} from '../dtos/Point';
+import {Location} from '../dtos/location';
+import {Unit} from '../dtos/unit';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HallCreationService {
+
+  initialized = false;
+  edited = false;
+
+  hall: Hall = new Hall(-1, -1, 'xy', new Location(3, 'yz', 'yz', 'yz', 'yz', 'yz'), new Point(5, 5));
+  hallName = 'xy';
+  hallSize: Point;
+
+  seats: Point[] = [];
+  aisles: Unit[] = [];
+  sectors: Unit[] = [];
+  point1: Point = new Point(1, 1);
+  point2: Point = new Point(1, 2);
+  unit1: Unit = new Unit(-1, -1, '', this.point1, this.point1, 1);
+
+  constructor() {
+    this.completeInitializing();
+  }
+
+  // ends initialization process and sets initialized to true
+  completeInitializing(): void {
+    this.hallName = 'hall_1';
+    this.hallSize = new Point(10, 10);
+    for (let i = 1; i < this.hallSize.coordinateX + 1; i++) {
+      for (let j = 1; j < this.hallSize.coordinateY + 1; j++) {
+        this.seats.push(new Point(i, j));
+      }
+    }
+    this.initialized = true;
+  }
+
+  // ends editing process and sets edited to true
+  completeEditing(): void {
+    this.edited = true;
+  }
+
+  // ends sector creation process and calls saveHall()
+  completeSectors(): void {
+  }
+
+  // saves hall and all units to db
+  saveHall(): void {
+  }
+
+  getHallSize(): Point {
+    return this.hallSize;
+  }
+
+  getSeats(): Point[] {
+    return this.seats;
+  }
+}
