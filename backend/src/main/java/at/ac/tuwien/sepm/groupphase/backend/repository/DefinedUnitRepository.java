bd6611e9dfcdd9e022a608f534a66212a4f2ad78
Benjamin Zinschitz <b.zinschitz@gmail.com>
2019-05-11 16:31:02 +0200

    [issue #35689] Add data generator for DefinedUnit

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/DefinedUnitRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/DefinedUnitRepository.java
@@ -1,8 +1,13 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.DefinedUnit;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Performance;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DefinedUnitRepository extends JpaRepository<DefinedUnit, Long> {}
+public interface DefinedUnitRepository extends JpaRepository<DefinedUnit, Long> {
+  List<DefinedUnit> findAllByPerformanceAndCapacityFreeIsGreaterThan(
+      Performance performance, int capacityFree);
+}
