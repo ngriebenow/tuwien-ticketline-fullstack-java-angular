c1193beb6471ccc9fb0b65615f3f3731c6f929f6
Benjamin Zinschitz <b.zinschitz@gmail.com>
2019-05-11 16:30:40 +0200

    [issue #35689] Add data generator for Performance

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/PerformanceRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/PerformanceRepository.java
@@ -1,6 +1,10 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
+import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Performance;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PerformanceRepository extends JpaRepository<Performance, Long> {}
+public interface PerformanceRepository extends JpaRepository<Performance, Long> {
+  List<Performance> findAllByEvent(Event event);
+}
