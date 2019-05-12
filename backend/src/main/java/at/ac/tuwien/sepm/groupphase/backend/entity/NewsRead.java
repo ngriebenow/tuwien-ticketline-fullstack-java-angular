19d7c6bd636e0aa9358be68a26d8755844a052e0
Bernhard Lautner <e1426777@student.tuwien.ac.at>
2019-05-12 10:21:15 +0200

    [issue #36423] Configure NewsRead persistance

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/NewsRead.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/NewsRead.java
@@ -1,3 +1,79 @@
 package at.ac.tuwien.sepm.groupphase.backend.entity;
 
-public class NewsRead {}
+import java.util.Objects;
+import javax.persistence.Column;
+
+public class NewsRead {
+
+  @Column(nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private long newsId;
+
+  /** Construct the newsRead instance. */
+  public NewsRead() {}
+
+  private NewsRead(Builder builder) {
+    setUsername(builder.username);
+    setNewsId(builder.newsId);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    NewsRead newsRead = (NewsRead) o;
+    return Objects.equals(username, newsRead.username)
+        && Objects.equals(newsId, newsRead.newsId);
+
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(username, newsId);
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public long getNewsId() {
+    return newsId;
+  }
+
+  public void setNewsId(long newsId) {
+    this.newsId = newsId;
+  }
+
+  public static final class Builder {
+
+    private String username;
+    private long newsId;
+
+    public Builder() {
+    }
+
+    public Builder username(String val) {
+      username = val;
+      return this;
+    }
+
+    public Builder newsId(long val) {
+      newsId = val;
+      return this;
+    }
+
+    public NewsRead build() {
+      return new NewsRead(this);
+    }
+  }
+}
