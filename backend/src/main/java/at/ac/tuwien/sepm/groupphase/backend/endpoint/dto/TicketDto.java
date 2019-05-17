1dc763965326c96630ba1949737fee9156383e81
Benjamin Zinschitz <b.zinschitz@gmail.com>
2019-05-17 22:45:03 +0200

    [issue #35709] Add builder, toString, equals and hashCode to TicketDto

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/TicketDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/TicketDto.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 
+import com.google.common.base.Objects;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
@@ -20,6 +21,16 @@ public class TicketDto {
 
   @ApiModelProperty("The Id of the performance the ticket is for")
   private Long performanceId;
+
+  public TicketDto() {}
+
+  private TicketDto(Builder builder) {
+    setId(builder.id);
+    setPriceInCents(builder.priceInCents);
+    setTitle(builder.title);
+    setInvoiceId(builder.invoiceId);
+    setPerformanceId(builder.performanceId);
+  }
 
   public Long getId() {
     return id;
@@ -59,5 +70,83 @@ public class TicketDto {
 
   public void setPerformanceId(Long performanceId) {
     this.performanceId = performanceId;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    TicketDto ticketDto = (TicketDto) obj;
+    return priceInCents == ticketDto.priceInCents
+        && Objects.equal(id, ticketDto.id)
+        && Objects.equal(title, ticketDto.title)
+        && Objects.equal(invoiceId, ticketDto.invoiceId)
+        && Objects.equal(performanceId, ticketDto.performanceId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id, priceInCents, title, invoiceId, performanceId);
+  }
+
+  @Override
+  public String toString() {
+    return "TicketDto{"
+        + "id="
+        + id
+        + ", priceInCents="
+        + priceInCents
+        + ", title='"
+        + title
+        + '\''
+        + ", invoiceId="
+        + invoiceId
+        + ", performanceId="
+        + performanceId
+        + '}';
+  }
+
+  public static final class Builder {
+
+    private Long id;
+    private int priceInCents;
+    private String title;
+    private Long invoiceId;
+    private Long performanceId;
+
+    public Builder() {}
+
+    public Builder id(Long val) {
+      id = val;
+      return this;
+    }
+
+    public Builder priceInCents(int val) {
+      priceInCents = val;
+      return this;
+    }
+
+    public Builder title(String val) {
+      title = val;
+      return this;
+    }
+
+    public Builder invoiceId(Long val) {
+      invoiceId = val;
+      return this;
+    }
+
+    public Builder performanceId(Long val) {
+      performanceId = val;
+      return this;
+    }
+
+    public TicketDto build() {
+      return new TicketDto(this);
+    }
   }
 }
