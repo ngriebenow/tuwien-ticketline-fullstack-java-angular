aa9faecc10da085fa4fd6e01488f5201253ec647
Nicolas Griebenow <nicolas.griebenow@gmx.at>
2019-05-21 23:45:45 +0200

    [issue #37950] Add price range to event detail site in frontend

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/DefinedUnitDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/DefinedUnitDataGenerator.java
@@ -61,7 +61,8 @@ public class DefinedUnitDataGenerator implements DataGenerator<DefinedUnit> {
     for (Event event : eventRepository.findAll()) {
       Hall hall = event.getHall();
       List<Unit> units = unitRepository.findAllByHall(hall);
-      List<PriceCategory> priceCategories = priceCategoryRepository.findAllByEvent(event);
+      List<PriceCategory> priceCategories = priceCategoryRepository
+          .findAllByEventOrderByPriceInCentsAsc(event);
       priceCategories.sort(Comparator.comparingInt(PriceCategory::getPriceInCents));
 
       int maxY = hall.getBoundaryPoint().getCoordinateY();
