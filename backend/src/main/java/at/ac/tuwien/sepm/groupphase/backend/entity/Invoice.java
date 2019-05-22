f1f43701e73058b3ea5fb5bfe8cb18b825829990
Benjamin Zinschitz <b.zinschitz@gmail.com>
2019-05-22 13:04:24 +0200

    [issue #36041] Configure all ManyToOne relations with lazy fetch strategy

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Invoice.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Invoice.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -40,7 +41,7 @@ public class Invoice {
   @Column(nullable = true)
   private LocalDate paidAt;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(nullable = false)
   private Client client;
 
