12a358bc073484dd27384b09790b3070f066e156
Nicolas Griebenow <nicolas.griebenow@gmx.at>
2019-05-26 23:23:58 +0200

    [issue #35694] Add sectors to units

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/UnitDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/UnitDataGenerator.java
@@ -26,6 +26,14 @@ public class UnitDataGenerator implements DataGenerator<Unit> {
   private UnitRepository unitRepository;
   private HallRepository hallRepository;
 
+  private static final int SECTOR_MIN_SIZE_X = 2;
+  private static final int SECTOR_MIN_SIZE_Y = 2;
+  private static final int SECTOR_MAX_SIZE_X = 4;
+  private static final int SECTOR_MAX_SIZE_Y = 4;
+
+  private static final int SECTOR_CAPACITY_MIN = 5;
+  private static final int SECTOR_CAPACITY_MAX = 20;
+
   @Autowired
   public UnitDataGenerator(UnitRepository unitRepository, HallRepository hallRepository) {
     this.unitRepository = unitRepository;
@@ -40,16 +48,54 @@ public class UnitDataGenerator implements DataGenerator<Unit> {
     for (Hall hall : hallRepository.findAll()) {
 
       generatedUnits.clear();
+
+      boolean[][] occupied = new boolean[
+          hall.getBoundaryPoint().getCoordinateX()] [
+              hall.getBoundaryPoint().getCoordinateY()];
+
+
       for (int i = 0; i < hall.getBoundaryPoint().getCoordinateY(); i++) {
         for (int j = 0; j < hall.getBoundaryPoint().getCoordinateX(); j++) {
-          generatedUnits.add(
-              new Unit.Builder()
-                  .name(String.format("Reihe %d Sitz %d", j + 1, i + 1))
-                  .lowerBoundary(new Point(j + 1, i + 1))
-                  .upperBoundary(new Point(j + 1, i + 1))
-                  .capacity(1)
-                  .hall(hall)
-                  .build());
+
+          int sectorSizeX = FAKER.random().nextInt(SECTOR_MIN_SIZE_X,SECTOR_MAX_SIZE_X);
+          int sectorSizeY = FAKER.random().nextInt(SECTOR_MIN_SIZE_Y,SECTOR_MAX_SIZE_Y);
+
+          if (!occupied[i][j]) {
+
+            boolean feasible = false;
+            for (int k = i; k < i + sectorSizeX; k++) {
+              for(int l = 0; l < j + sectorSizeY; l++) {
+                feasible |= occupied[k][l];
+              }
+            }
+
+            if (feasible && FAKER.random().nextInt(-500,500) < -200) {
+
+              for (int k = i; k < i + sectorSizeX; k++) {
+                for(int l = 0; l < j + sectorSizeY; l++) {
+                  occupied[k][l] = true;
+                }
+              }
+
+              generatedUnits.add(
+                  new Unit.Builder()
+                      .name("Sektor " + FAKER.commerce().productName())
+                      .lowerBoundary(new Point(j + 1, i + 1))
+                      .upperBoundary(new Point(j + sectorSizeX, i + sectorSizeY))
+                      .capacity(FAKER.random().nextInt(SECTOR_CAPACITY_MIN, SECTOR_CAPACITY_MAX))
+                      .hall(hall)
+                      .build());
+            } else {
+              generatedUnits.add(
+                  new Unit.Builder()
+                      .name(String.format("Reihe %d Sitz %d", j + 1, i + 1))
+                      .lowerBoundary(new Point(j + 1, i + 1))
+                      .upperBoundary(new Point(j + 1, i + 1))
+                      .capacity(1)
+                      .hall(hall)
+                      .build());
+            }
+          }
         }
       }
       unitRepository.saveAll(generatedUnits);
