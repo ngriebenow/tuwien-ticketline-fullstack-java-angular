43ab6443b228f016b8f88c8f33db4d3f8ca6b590
Bernhard Lautner <e1426777@student.tuwien.ac.at>
2019-06-03 17:39:02 +0200

    [issue #39223] Fix frontend tests

--- frontend/src/app/services/picture-transfer.service.spec.ts
+++ frontend/src/app/services/picture-transfer.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PictureTransferService } from './picture-transfer.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {Globals} from '../global/globals';
 
 describe('PictureTransferService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+    ],
+    providers: [
+      Globals,
+    ],
+  }));
 
   it('should be created', () => {
     const service: PictureTransferService = TestBed.get(PictureTransferService);
