0ea22473e2d1efbb3f3414f55f6750dbd9735903
Bernhard Lautner <e1426777@student.tuwien.ac.at>
2019-06-04 15:26:56 +0200

    [issue #39229] Integrate Lightbox for Newspictures

--- frontend/src/app/components/picture-display/picture-display.component.ts
+++ frontend/src/app/components/picture-display/picture-display.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Picture } from '../../dtos/picture';
 import {PictureService} from '../../services/picture.service';
+import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-picture-display',
@@ -10,8 +11,15 @@ import {PictureService} from '../../services/picture.service';
 export class PictureDisplayComponent implements OnInit {
   @Input() ids: number[] = new Array();
   pictures: Picture[];
-  constructor(private pictureService: PictureService) {
+  private albums: any[];
+  private promisesLoaded: any[];
+  private promisesConverted: any[];
+  constructor(private pictureService: PictureService,
+              private lightbox: Lightbox) {
     this.pictures = new Array();
+    this.albums = new Array();
+    this.promisesLoaded = new Array();
+    this.promisesConverted = new Array();
   }
   ngOnInit() {
     this.loadAllPictures();
@@ -23,6 +31,22 @@ export class PictureDisplayComponent implements OnInit {
     for (const id of this.ids) {
       this.loadPictureById(id);
     }
+    Promise.all(this.promisesLoaded).then(function (resultLoaded) {
+      Promise.all(this.promisesConverted).then(function (resultConverted) {
+        const sortedPicture = this.pictures.sort((a, b) => a.id - b.id);
+        for (const picture of sortedPicture) {
+          const src = picture.data;
+          const caption = '';
+          const thumb = picture.data;
+          const album = {
+            src: src,
+            caption: caption,
+            thumb: thumb
+          };
+          this.albums.push(album);
+        }
+      }.bind(this));
+    }.bind(this));
   }
   /**
    * load a picture with given id from service and append it to pictures.
@@ -30,11 +54,11 @@ export class PictureDisplayComponent implements OnInit {
    */
   loadPictureById(id: number) {
     console.log('Load picture with id ' + id);
-    this.pictureService.getPicture(id).subscribe(data => {
-      this.appendPictureFromBlob(id, data);
-    }, error => {
-      console.log(error);
-    });
+    const promise = this.pictureService.getPicture(id).toPromise();
+    promise.then(function (result) {
+      this.appendPictureFromBlob(id, result);
+    }.bind(this));
+    this.promisesLoaded.push(promise);
   }
   /**
    * convert the blob from db and append the result to pictures.
@@ -43,10 +67,15 @@ export class PictureDisplayComponent implements OnInit {
    */
   appendPictureFromBlob(id: number, picture: Blob) {
     const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      this.pictures.push(new Picture(id, null, reader.result));
-    }, false);
-
+    const promise = new Promise(function (resolve) {
+      reader.addEventListener('load', () => {
+        resolve(new Picture(id, null, reader.result));
+      }, false);
+    }.bind(this));
+    promise.then(function (result) {
+      this.pictures.push(result);
+    }.bind(this));
+    this.promisesConverted.push(promise);
     if (picture) {
       reader.readAsDataURL(picture);
     }
@@ -55,8 +84,17 @@ export class PictureDisplayComponent implements OnInit {
    * return sorted pictures array.
    * @return picture array sorted by id ascending
    */
-  getPicturesSortedById(): Picture[] {
-    return this.pictures.sort((a, b) => a.id - b.id);
+  getAlbums(): any[] {
+    return this.albums;
+  }
+
+  open(index: number): void {
+    // open lightbox
+    this.lightbox.open(this.albums, index);
+  }
+  close(): void {
+    // close lightbox programmatically
+    this.lightbox.close();
   }
 
 }
