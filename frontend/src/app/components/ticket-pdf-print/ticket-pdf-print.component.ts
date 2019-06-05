c276ecf5b1c629ef9dcfa3987c35931744bb4701
Bernhard Lautner <e1426777@student.tuwien.ac.at>
2019-06-05 18:24:22 +0200

    [issue #39241] Implement prototype for frontend pdf ticket print

--- frontend/src/app/components/ticket-pdf-print/ticket-pdf-print.component.ts
+++ frontend/src/app/components/ticket-pdf-print/ticket-pdf-print.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Ticket} from '../../dtos/ticket';
+import * as jsPDF from 'jspdf'; // star import is apparently required here, see https://github.com/MrRio/jsPDF/issues/956
 
 @Component({
   selector: 'app-ticket-pdf-print',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-pdf-print.component.scss']
 })
 export class TicketPdfPrintComponent implements OnInit {
-
-  constructor() { }
-
+  dummyTicket: Ticket;
+  constructor() {
+    this.dummyTicket = new Ticket(
+    0,
+    'Title',
+    'Event A',
+    'Performance 1 Event A',
+    new Date().toISOString(),
+    'Price Category 1',
+    10000,
+    'Wien Stadthalle',
+    'R 104',
+    100,
+    3);
+  }
   ngOnInit() {
+  }
+  printPDF(ticket: Ticket) {
+    const doc = new jsPDF();
+    doc.text(10, 10, ticket.id.toString());
+    doc.text(10, 20, ticket.title);
+    doc.text(10, 30, ticket.eventName);
+    doc.text(10, 40, ticket.performanceName);
+    doc.text(10, 50, ticket.startAt.toLocaleUpperCase());
+    doc.text(10, 60, ticket.priceCategoryName);
+    doc.text(10, 70, ticket.priceInCents.toString());
+    doc.text(10, 80, ticket.locationName);
+    doc.text(10, 90, ticket.hallName);
+    doc.text(10, 100, ticket.definedUnitId.toString());
+    doc.text(10, 110, ticket.performanceId.toString());
+    doc.save('Ticket-' + ticket.id + '.pdf');
   }
 
 }
